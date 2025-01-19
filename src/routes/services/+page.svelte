65e2088bbd8f9cd37b3ab8d5cbb4bdfca3e26b6e
jbefus <jurij.befus@gmail.com>
2025-01-20 00:38:35 +0100

    add two more page (wip)

--- src/routes/services/+page.svelte
+++ src/routes/services/+page.svelte
@@ -1,1 +1,12 @@
+<script lang="ts">
+	let { data } = $props();
 	console.log('data', data);
+</script>
+
+<h1>Services</h1>
+
+<ul>
+	{#each data.services as service}
+		<li>{service.service_name}</li>
+	{/each}
+</ul>
