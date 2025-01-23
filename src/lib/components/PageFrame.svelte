1af75b1033020ede2dc0f46dacd9ed4a6a3b75ba
jbefus <jurij.befus@gmail.com>
2025-01-23 23:49:31 +0100

    cleanup

---
+++ src/lib/components/PageFrame.svelte
@@ -0,0 +1,1 @@
+<div class="m-8 max-w-screen-md"><slot /></div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,15 +1,10 @@
 <script lang="ts">
-	import FancyImage from '$lib/components/FancyImage.svelte';
-	import imgSrc from '$lib/assets/jurij_lotti_no_bg.png';
 	import { fly } from 'svelte/transition';
 	const ENVIRONMENT = import.meta.env.VITE_ENVIRONMENT || 'local';
 </script>
 
 <div class="flex w-full flex-row justify-around">
-	<div class="h-1/2 w-2/3">
-		<FancyImage {imgSrc} />
-	</div>
-	<div in:fly={{ y: 200, duration: 2000 }} class="right w-1/3">
+	<div in:fly={{ y: 200, duration: 2000 }} class="mx-5 max-w-screen-md">
 		<h2 class="my-3 text-xl font-bold">Welcome, friend</h2>
 		<p class="">
 			Welcome to my page. I don't know how you found this place, and to be honest, it doesn't
@@ -17,8 +12,8 @@
 		</p>
 		<br />
 		<p>
-			You can look at what I can offer and if it might benefit you. You can
-			check out my tools, or just stroll around and look at what else the site has to offer.
+			You can look at what I can offer and if it might benefit you. You can check out my tools, or
+			just stroll around and look at what else the site has to offer.
 		</p>
 	</div>
 </div>
