79699ea0c9197c143d9f9c45aa8c561f652568ca
jbefus <jurij.befus@gmail.com>
2025-01-23 23:49:04 +0100

    create flowbite based navbar

--- src/lib/components/Navbar.svelte
+++ src/lib/components/Navbar.svelte
@@ -1,10 +1,24 @@
 <script lang="ts">
+	import {
+		Navbar,
+		NavBrand,
+		NavLi,
+		NavUl,
+		NavHamburger,
+		Dropdown,
+		DropdownItem,
+	} from 'flowbite-svelte';
+	import { ChevronDownOutline } from 'flowbite-svelte-icons';
+	import imgSrc from '$lib/assets/jurij_lotti_no_bg.png';
 	import { page } from '$app/state';
+	const activeClass = 'font-extrabold  text-zinc-900  dark:text-amber-100';
+
 	type NavItem = {
-		route: string;
+		route?: string;
 		text: string;
 		children?: NavItem[];
 	};
+
 	const navItems: NavItem[] = [
 		{
 			route: '/about',
@@ -15,26 +29,35 @@
 			text: 'My Work',
 		},
 		{
-			route: '/todo',
-			text: 'Todo App',
+			text: 'Tools',
+			children: [{ text: 'Tomatoes', route: '/tomatoes' }],
 		},
 	];
 </script>
 
-<nav class="navbar justify-between">
-	<div class="flex w-full flex-row justify-between p-3">
-		<div>
-			{#each navItems as item}
-				<a href={item.route} class="mx-3 {item.route == page.url.pathname ? 'active' : ''}">
-					{item.text}
-				</a>
-			{/each}
-		</div>
-	</div>
-</nav>
-
-<style lang="postcss">
-	.active {
-		@apply font-extrabold text-blue-900 dark:text-red-200;
-	}
-</style>
+<Navbar>
+	<NavBrand href="/">
+		<img src={imgSrc} class="me-3 h-6 sm:h-9" alt="jurij and lotti" />
+		<span class="self-center whitespace-nowrap text-xl font-semibold dark:text-white">
+			jbefus.de
+		</span>
+	</NavBrand>
+	<NavHamburger />
+	<NavUl activeUrl={page.url.pathname} {activeClass}>
+		{#each navItems as item}
+			{#if !!item.children?.length}
+				<NavLi class="cursor-pointer">
+					{item.text}<ChevronDownOutline
+						class="text-primary-800 ms-2 inline h-6 w-6 dark:text-white" />
+				</NavLi>
+				<Dropdown class="z-20 w-44">
+					{#each item.children as itemChild}
+						<DropdownItem href={itemChild.route}>{itemChild.text}</DropdownItem>
+					{/each}
+				</Dropdown>
+			{:else}
+				<NavLi href={item.route}>{item.text}</NavLi>
+			{/if}
+		{/each}
+	</NavUl>
+</Navbar>
