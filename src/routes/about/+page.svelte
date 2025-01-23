9108943ad268b3e234011c66218da72464f09226
jbefus <jurij.befus@gmail.com>
2025-01-23 23:33:46 +0100

    updated about

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,5 +1,5 @@
 <h1>About me</h1>
 <p>
 	Hey, my name is Jurij. I use this site mainly as a playground for gh actions, deployment scripts,
-	DNS tests and so on.
+	DNS tests and so on. In the coming weeks, I'll add some tooling and a CV.
 </p>
