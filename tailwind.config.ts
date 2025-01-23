7b37b45c104c457567195bbd8de442e0e787723d
jbefus <jurij.befus@gmail.com>
2025-01-23 23:46:26 +0100

    add flowbite

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,13 +1,17 @@
 import containerQueries from '@tailwindcss/container-queries';
 import typography from '@tailwindcss/typography';
+import flowbitePlugin from 'flowbite/plugin';
 import type { Config } from 'tailwindcss';
 
 export default {
-	content: ['./src/**/*.{html,js,svelte,ts}'],
+	content: [
+		'./src/**/*.{html,js,svelte,ts}',
+		'./node_modules/flowbite-svelte/**/*.{html,js,svelte,ts}',
+	],
 	darkMode: 'media',
 	theme: {
 		extend: {},
 	},
 
-	plugins: [typography, containerQueries],
+	plugins: [typography, containerQueries, flowbitePlugin],
 } satisfies Config;
